671085c40863342ee38ada4ae8be73d64ced965e
Manoj Patra <patra.manoj0@gmail.com>
2022-12-06 13:11:14 +0530

    complete Transitions boilerplate

--- src/Transitions/boilerplate/AnimatedCard.tsx
+++ src/Transitions/boilerplate/AnimatedCard.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable max-len */
 import { StyleSheet, Dimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
+import { mix } from "react-native-redash";
 
 import type { Cards } from "../../components";
 import { Card, StyleGuide } from "../../components";
 
 const { width } = Dimensions.get("window");
 const origin = -(width / 2 - StyleGuide.spacing * 2);
+/**
+ * https://stackoverflow.com/questions/53676723/react-native-what-is-the-difference-between-stylesheet-absolutefill-and-styl
+ */
 const styles = StyleSheet.create({
   overlay: {
     ...StyleSheet.absoluteFillObject,
@@ -16,22 +21,29 @@ const styles = StyleSheet.create({
 });
 
 interface AnimatedCardProps {
-  toggled: boolean;
+  transition: Animated.SharedValue<number>;
   index: number;
   card: Cards;
 }
 
-export const AnimatedCard = ({ card, toggled, index }: AnimatedCardProps) => {
-  const alpha = toggled ? ((index - 1) * Math.PI) / 6 : 0;
-  const style = {
-    transform: [
-      { translateX: origin },
-      { rotate: `${alpha}rad` },
-      { translateX: -origin },
-    ],
-  };
+export const AnimatedCard = ({
+  transition,
+  index,
+  card,
+}: AnimatedCardProps) => {
+  const animatedStyle = useAnimatedStyle(() => {
+    // x * (1 - value) + y * value
+    const alpha = mix(transition.value, 0, ((index - 1) * Math.PI) / 6);
+    return {
+      transform: [
+        { translateX: origin },
+        { rotate: `${alpha}rad` },
+        { translateX: -origin },
+      ],
+    };
+  });
   return (
-    <Animated.View key={card} style={[styles.overlay, style]}>
+    <Animated.View key={card} style={[styles.overlay, animatedStyle]}>
       <Card {...{ card }} />
     </Animated.View>
   );
--- src/Transitions/boilerplate/Transitions.tsx
+++ src/Transitions/boilerplate/Transitions.tsx
@@ -1,5 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import type {
+  WithSpringConfig,
+  WithTimingConfig,
+} from "react-native-reanimated";
+import {
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
 import { Button, StyleGuide, cards } from "../../components";
 
@@ -13,17 +23,57 @@ const styles = StyleSheet.create({
   },
 });
 
-export const UseTransition = () => {
-  const [toggled, setToggle] = useState(false);
+const useSpring = (state: number | boolean, config?: WithSpringConfig) => {
+  const value = useSharedValue(0);
+
+  useEffect(() => {
+    if (typeof state === "number") {
+      value.value = state;
+    } else {
+      value.value = state ? 1 : 0;
+    }
+  }, [state, value]);
+
+  return useDerivedValue(() => {
+    return withSpring(value.value, config);
+  });
+};
+
+const useTiming = (state: number | boolean, config?: WithTimingConfig) => {
+  const value = useSharedValue(0);
+
+  useEffect(() => {
+    if (typeof state === "number") {
+      value.value = state;
+    } else {
+      value.value = state ? 1 : 0;
+    }
+  }, [state, value]);
+
+  return useDerivedValue(() => {
+    return withTiming(value.value, config);
+  });
+};
+
+export const Transitions = () => {
+  const toggled = useSharedValue(0); // 0 => false
+  // const transition = useTiming(toggled, {
+  //   duration: 600,
+  // });
+
+  const transition = useDerivedValue(() => {
+    return withSpring(toggled.value);
+  }, [toggled]);
+
   return (
     <View style={styles.container}>
       {cards.slice(0, 3).map((card, index) => (
-        <AnimatedCard key={card} {...{ index, card, toggled }} />
+        <AnimatedCard key={card} {...{ index, card, transition }} />
       ))}
       <Button
-        label={toggled ? "Reset" : "Start"}
+        label={toggled.value ? "Reset" : "Start"}
         primary
-        onPress={() => setToggle((prev) => !prev)}
+        onPress={() => (toggled.value = !toggled.value ? 1 : 0)}
       />
     </View>
   );
