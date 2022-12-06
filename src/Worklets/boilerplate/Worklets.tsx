1f682cb25c1d9dfdfd4602a380b165e7131d1c2d
Manoj Patra <patra.manoj0@gmail.com>
2022-12-06 13:13:43 +0530

    complete worklet boilerplate

--- src/Worklets/boilerplate/Worklets.tsx
+++ src/Worklets/boilerplate/Worklets.tsx
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from "react-native";
+import { runOnJS, runOnUI } from "react-native-reanimated";
 
 import { Button } from "../../components";
 
@@ -10,14 +11,24 @@ const styles = StyleSheet.create({
   },
 });
 
-const sayHello = () => {
+const sayHello = (cb: () => void) => {
+  "worklet";
   console.log("Hello from the JS thread!");
+  runOnJS(cb)();
 };
 
 export const Worklets = () => {
   return (
     <View style={styles.container}>
-      <Button label="Say Hello" primary onPress={() => sayHello()} />
+      <Button
+        label="Say Hello"
+        primary
+        onPress={() =>
+          runOnUI(sayHello)(() =>
+            console.log("We called back to the JS thread")
+          )
+        }
+      />
     </View>
   );
 };
