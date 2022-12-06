793bcab27b50ca1c3c03c0a0ee0da6acb2891979
Manoj Patra <patra.manoj0@gmail.com>
2022-12-06 13:10:47 +0530

    complete PanGesture boilerplate

--- src/PanGesture/boilerplate/PanGesture.tsx
+++ src/PanGesture/boilerplate/PanGesture.tsx
@@ -1,6 +1,15 @@
 import { View, StyleSheet } from "react-native";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+} from "react-native-reanimated";
+import { clamp, withBouncing } from "react-native-redash";
 
-import { Card, Cards } from "../../components";
+import { Card, Cards, CARD_HEIGHT, CARD_WIDTH } from "../../components";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +23,58 @@ interface GestureProps {
 }
 
 export const PanGesture = ({ width, height }: GestureProps) => {
-  console.log({ width, height });
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const boundX = width - CARD_WIDTH;
+  const boundY = height - CARD_HEIGHT;
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { offsetX: number; offsetY: number }
+  >({
+    onStart: (_, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
+    },
+    onEnd: (event) => {
+      translateX.value = withBouncing(
+        withDecay({
+          velocity: event.velocityX,
+        }),
+        0,
+        boundX
+      );
+      translateY.value = withBouncing(
+        withDecay({
+          velocity: event.velocityY,
+        }),
+        0,
+        boundY
+      );
+    },
+  });
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Card card={Cards.Card1} />
+      <PanGestureHandler {...{ onGestureEvent }}>
+        <Animated.View {...{ style }}>
+          <Card card={Cards.Card1} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
