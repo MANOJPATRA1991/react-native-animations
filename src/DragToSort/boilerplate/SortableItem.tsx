55b528323e67c714504c6cfa8586f2c7bad5d124
Manoj Patra <patra.manoj0@gmail.com>
2023-01-01 11:57:11 +0530

    complete drag and sort

--- src/DragToSort/boilerplate/SortableItem.tsx
+++ src/DragToSort/boilerplate/SortableItem.tsx
@@ -1,5 +1,87 @@
-import { View } from "react-native";
+import type { ReactElement } from "react";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-export const SortableItem = () => {
-  return <View />;
+interface SortableItemProps {
+  index: number;
+  offsets: { y: Animated.SharedValue<number> }[];
+  children: ReactElement;
+  item: { width: number; height: number };
+  activeCard: Animated.SharedValue<number>;
+}
+
+export const SortableItem = ({
+  index,
+  offsets,
+  children,
+  item: { width, height },
+  activeCard,
+}: SortableItemProps) => {
+  const isGestureActive = useSharedValue(false);
+  const currentOffset = offsets[index];
+
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { offsetY: number }
+  >({
+    onStart: (_, ctx) => {
+      isGestureActive.value = true;
+      activeCard.value = index;
+      ctx.offsetY = currentOffset.y.value;
+    },
+    onActive: ({ translationX, translationY }, ctx) => {
+      x.value = translationX;
+      y.value = translationY + ctx.offsetY;
+      const offsetY = Math.round(y.value / height) * height;
+      offsets.forEach((offset, i) => {
+        if (offset.y.value === offsetY && index !== i) {
+          offset.y.value = currentOffset.y.value;
+          currentOffset.y.value = offsetY;
+        }
+      });
+    },
+    onEnd: () => {
+      isGestureActive.value = false;
+      x.value = withSpring(0);
+      y.value = withSpring(currentOffset.y.value);
+    },
+  });
+
+  const translateY = useDerivedValue(() => {
+    if (isGestureActive.value) {
+      return y.value;
+    } else {
+      return withSpring(currentOffset.y.value);
+    }
+  });
+
+  const style = useAnimatedStyle(() => ({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width,
+    height,
+    zIndex: activeCard.value === index ? 100 : 1,
+    transform: [
+      { translateY: translateY.value },
+      { translateX: x.value },
+      { scale: withSpring(isGestureActive.value ? 1.05 : 1) },
+    ],
+  }));
+
+  return (
+    <PanGestureHandler {...{ onGestureEvent }}>
+      <Animated.View style={style}>{children}</Animated.View>
+    </PanGestureHandler>
+  );
 };
--- src/DragToSort/boilerplate/SortableList.tsx
+++ src/DragToSort/boilerplate/SortableList.tsx
@@ -1,12 +1,33 @@
 import type { ReactElement } from "react";
 import React from "react";
 import { ScrollView } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+
+import { SortableItem } from "./SortableItem";
 
 interface SortableListProps {
   children: ReactElement[];
   item: { width: number; height: number };
 }
 
-export const SortableList = ({ children }: SortableListProps) => {
-  return <ScrollView>{children}</ScrollView>;
+export const SortableList = ({
+  children,
+  item: { width, height },
+}: SortableListProps) => {
+  const activeCard = useSharedValue(-1);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const offsets = children.map((_, i) => ({ y: useSharedValue(height * i) }));
+
+  return (
+    <ScrollView contentContainerStyle={{ height: height * children.length }}>
+      {children.map((child, index) => (
+        <SortableItem
+          key={index}
+          {...{ offsets, index, item: { width, height }, activeCard }}
+        >
+          {child}
+        </SortableItem>
+      ))}
+    </ScrollView>
+  );
 };
