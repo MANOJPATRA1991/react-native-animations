d0f4a7fa958ee70b28fe01d2c8b236ece8d8940a
Manoj Patra <patra.manoj0@gmail.com>
2023-01-01 12:24:38 +0530

    complet bezier curves

--- src/Bezier/boilerplate/ControlPoint.tsx
+++ src/Bezier/boilerplate/ControlPoint.tsx
@@ -1,24 +1,59 @@
-import { View } from "react-native";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { clamp } from "react-native-redash";
 
 export const CONTROL_POINT_RADIUS = 20;
 
 interface ControlPointProps {
-  x: number;
-  y: number;
+  x: Animated.SharedValue<number>;
+  y: Animated.SharedValue<number>;
   min: number;
   max: number;
 }
 
-export const ControlPoint = ({}: ControlPointProps) => {
+type Offset = {
+  x: number;
+  y: number;
+};
+
+export const ControlPoint = ({ x, y, min, max }: ControlPointProps) => {
+  const style = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: x.value - CONTROL_POINT_RADIUS },
+      { translateY: y.value - CONTROL_POINT_RADIUS },
+    ],
+  }));
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    Offset
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = x.value;
+      ctx.y = y.value;
+    },
+    onActive: ({ translationX, translationY }, ctx) => {
+      x.value = clamp(ctx.x + translationX, min, max);
+      y.value = clamp(ctx.y + translationY, min, max);
+    },
+  });
+
   return (
-    <View
-      style={[
-        {
-          position: "absolute",
-          width: CONTROL_POINT_RADIUS * 2,
-          height: CONTROL_POINT_RADIUS * 2,
-        },
-      ]}
-    />
+    <PanGestureHandler {...{ onGestureEvent }}>
+      <Animated.View
+        style={[
+          {
+            position: "absolute",
+            width: CONTROL_POINT_RADIUS * 2,
+            height: CONTROL_POINT_RADIUS * 2,
+          },
+          style,
+        ]}
+      />
+    </PanGestureHandler>
   );
 };
